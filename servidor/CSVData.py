630fc6eaad644495a5f7b3043899a88ff5da039e
lewatoto <lewatoto@gmail.com>
2017-04-30 13:36:59 -0600

    obtener datos del archivos csv

--- servidor/CSVData.py
+++ servidor/CSVData.py
@@ -2,7 +2,7 @@ import csv
 with open('MODIS_C6_Central_America_24h.csv', 'rb') as f:
     reader = csv.reader(f)
     datos = list(reader)
-    
+
 Latitude =[]
 Longitude=[]
 Brightness =[]
@@ -16,21 +16,31 @@ Version=[]
 Bright_t31=[]
 Frp=[]
 Daynight=[]
-
-print datos
+archivo = open('datos.csv','w')
+archivo.write('Lat,Lng,popup  \n')
 datos.pop(0)
+a=0
 for i in datos:
+
     Latitude.append(i[0])
     Longitude.append(i[1])
-    Brightness.append(i[2])
+    Brightness.append(i[2])#t21
     Scan.append(i[3])
     Track.append(i[4])
     Acq_Date.append(i[5])
     Acq_Time.append(i[6])
-    Satellite.append(i[7])
-    Confidence.append(i[8])
+    Satellite.append(i[7])#tierra o agua
+    Confidence.append(i[8])#veracidad
     Version.append(i[9])
-    Bright_t31.append(i[10])
+    Bright_t31.append(i[10])#t31
     Frp.append(i[11])
     Daynight.append(i[12])
+    if(float(Latitude[a])>13.7381)and(float(Latitude[a])<17.937)and(float(Longitude[a])>-92.109)and(float(Longitude[a])<-88.285)and(int(Confidence[a])>60)and(float(Brightness[a])>330):
+        print Latitude[a]+','+Longitude[a]+','+Confidence[a]+','+Acq_Date[a]+'\n'
+        archivo.write(Latitude[a]+','+Longitude[a]+','+Acq_Date[a]+'\n')
+    a =a+1
 
+
+
+archivo.close()
+print 'Append finalizado'
--- servidor/static/js/alertafuego.js
+++ servidor/static/js/alertafuego.js
@@ -1,15 +1,17 @@
+var map = L.map('map').setView([15.753, -90.291], 8);
+
 $(function(){
   // initialize the map
-  var map = L.map('map').setView([15.753, -90.291], 8);
+  //var map = L.map('map').setView([15.753, -90.291], 8);
 
   // load a tile layer
   var capa = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       maxZoom: 13,
       minZoom: 8
-  });
-  map.addLayer(capa);
+  }).addTo(map);
 
+/*
   var kmlLayer = new L.KML('https://lewatoto.github.io/alertafuego/MODIS_C6_Central_America_24h.kml', {async: true});
     kmlLayer.on("loaded", function(e) {
       console.log(e);
@@ -17,6 +19,7 @@ $(function(){
     });
   map.addLayer(kmlLayer);
 
+*/
   /*
   $.ajax ({
     type:'GET',
@@ -33,4 +36,23 @@ $(function(){
         map.addLayer(kmlLayer);
     }
   });*/
+
+  $("#reportar").click(function(){
+    alert("reporte con éxito");
+    obtenerposicion();
+  });
 });
+
+function obtenerposicion(){
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(reporte);
+  } else {
+    alert("Geolocalización no soportada");
+  }
+}
+
+function reporte(posicion){
+  $('#lat').val(posicion.coords.latitude);
+  $('#long').val(posicion.coords.longitude);
+  var marker = L.marker([posicion.coords.latitude,posicion.coords.longitude]).addTo(map);
+};
